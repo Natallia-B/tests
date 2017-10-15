b392505a338cf10da5de012be279feebd0fd8f0b
Natallia-B <nokatami@Natallias-MacBook-Pro.local>
2017-10-15 17:16:59 +0300

    1.5

--- features/support/world.js
+++ features/support/world.js
@@ -3,13 +3,14 @@ let webdriver = require('selenium-webdriver');
 let {defineSupportCode} = require('cucumber');
 
 
-
 function CustomWorld() {
   this.driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
     global.By = webdriver.By;
     global.until = webdriver.until;
+    // driver.Window.maximize();
+    this.driver.manage().window().setSize(779, 431);
 }
 
 defineSupportCode(function({setWorldConstructor}) {
